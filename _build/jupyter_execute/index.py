52f29256836ca17f67e978edb2cf42609f0a1704
Nils <rata@zhaw.ch>
2021-09-23 09:14:39 +0200

    updatedQ

--- _build/jupyter_execute/index.py
+++ _build/jupyter_execute/index.py
@@ -3,6 +3,6 @@
 
 # # Einleitung
 # 
-# Willkommen im Kurs *[Angewandte Geoinformatik](https://studienplaner.lsfm.zhaw.ch/module/angewandte-geoinformatik)* der Zürcher Hochschule für Angewandte Wissenschaften (ZHAW). 
+# Willkommen im Kurs *[Angewandte Geoinformatik](https://studienplaner.lsfm.zhaw.ch/module/angewandte-geoinformatik)* der Zürcher Hochschule für Angewandte Wissenschaften (ZHAW). Wir nutzen diese Plattform (modul-agi.github.io) um euch Zugriff auf unsere unsere Übungsunterlagen und Theorieinputs zu gewährleisten. 
 # 
 # Der gesamte Quellcode um dieses Buch zu erstellen ist in dem folgenden github-repo verfügbar: [github.com/Modul-AGI](https://github.com/Modul-AGI/).
